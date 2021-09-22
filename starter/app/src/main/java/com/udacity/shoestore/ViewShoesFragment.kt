6ac19071fb4036658dc839c0c40f94cfaea3d8b3
Daniel Sullivan <thematr_x@yahoo.com>
2021-09-22 00:20:39 -0400

    Add placeholder fragments that represent all the destinations in the app. Add the fragments and their actions to a navigation file.

---
+++ starter/app/src/main/java/com/udacity/shoestore/ViewShoesFragment.kt
@@ -0,0 +1,24 @@
+package com.udacity.shoestore
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.udacity.shoestore.databinding.FragmentViewShoesBinding
+
+
+class ViewShoesFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val binding: FragmentViewShoesBinding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_view_shoes, container, false)
+        return binding.root
+    }
+
+}
