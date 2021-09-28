5c88aa525129830c559d96533afc448a1d5ad2bf
Daniel Sullivan <thematr_x@yahoo.com>
2021-09-28 15:36:09 -0400

    Display the shoes in the view shoes destination. This will shoe the built in shoes in addition to any shoe the user creates.

--- starter/app/src/main/java/com/udacity/shoestore/ViewShoesFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/ViewShoesFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -13,22 +15,25 @@ import androidx.navigation.fragment.navArgs
 import com.udacity.shoestore.databinding.FragmentViewShoesBinding
 import com.udacity.shoestore.models.Shoe
 import com.udacity.shoestore.models.ShoeViewModel
-import timber.log.Timber
 
 
 class ViewShoesFragment : Fragment() {
 
     private lateinit var viewModel: ShoeViewModel
+    lateinit var binding: FragmentViewShoesBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val binding: FragmentViewShoesBinding =
+        binding =
             DataBindingUtil.inflate(inflater, R.layout.fragment_view_shoes, container, false)
 
-        binding.newShoeActionButton.setOnClickListener {v:View -> v.findNavController().navigate(ViewShoesFragmentDirections.actionViewShoesFragmentToAddShoeFragment())}
+        binding.newShoeActionButton.setOnClickListener { v: View ->
+            v.findNavController()
+                .navigate(ViewShoesFragmentDirections.actionViewShoesFragmentToAddShoeFragment())
+        }
 
         viewModel = ViewModelProvider(this).get(ShoeViewModel::class.java)
 
@@ -36,11 +41,11 @@ class ViewShoesFragment : Fragment() {
         // the binding can observe LiveData updates
         binding.setLifecycleOwner(this)
 
-        viewModel.shoeList.observe(viewLifecycleOwner, Observer {shoes -> displayShoes(shoes) })
+        viewModel.shoeList.observe(viewLifecycleOwner, Observer { shoes -> displayShoes(shoes) })
 
         val viewShoesFragmentArgs by navArgs<ViewShoesFragmentArgs>()
 
-        if(viewShoesFragmentArgs.shoe != null) {
+        if (viewShoesFragmentArgs.shoe != null) {
             viewModel.addShoe(viewShoesFragmentArgs.shoe!!)
         }
 
@@ -49,6 +54,30 @@ class ViewShoesFragment : Fragment() {
 
     private fun displayShoes(shoes: List<Shoe>) {
         shoes.count()
+
+        val shoeLayout =
+            LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.MATCH_PARENT,
+            )
+
+        shoeLayout.bottomMargin = resources.getDimension(R.dimen.small_margin).toInt()
+
+        for (shoe in shoes) {
+
+            var shoeInfoText = TextView(context);
+            shoeInfoText.layoutParams = shoeLayout
+
+            shoeInfoText.text = String.format(
+                getString(R.string.shoe_info_format),
+                shoe.name,
+                shoe.company,
+                shoe.description,
+                shoe.size
+            )
+
+            binding.shoeListLayout.addView(shoeInfoText,0)
+        }
     }
 
 }
