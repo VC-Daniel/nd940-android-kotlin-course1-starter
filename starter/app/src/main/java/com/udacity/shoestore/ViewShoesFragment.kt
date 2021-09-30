c2a79781bf1411e454dc967b7e63bb4220c55f4d
Daniel Sullivan <thematr_x@yahoo.com>
2021-09-30 17:54:41 -0400

    remove unused import

--- starter/app/src/main/java/com/udacity/shoestore/ViewShoesFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/ViewShoesFragment.kt
@@ -2,6 +2,7 @@ package com.udacity.shoestore
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
