e0d710ff393f3362873bb604a1d50ce45072c295
Daniel Sullivan <thematr_x@yahoo.com>
2021-10-01 22:11:53 -0400

    Clean up code and add more comments.

--- starter/app/src/main/java/com/udacity/shoestore/models/LoginViewModel.kt
+++ starter/app/src/main/java/com/udacity/shoestore/models/LoginViewModel.kt
@@ -4,18 +4,23 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
+/** Facilitate logging in or creating a new account */
 class LoginViewModel : ViewModel() {
 
+    // These variables are connected with two way data binding to the UI so the user can specify
+    // account information for creating a new account
     val email = MutableLiveData<String>()
     val password = MutableLiveData<String>()
 
-    // Event which signifies a new account has been created successfully
     private val _eventCreatedAccount = MutableLiveData<Boolean>()
+
+    /** Event which signifies a new account has been created successfully. */
     val eventCreatedAccount: LiveData<Boolean>
         get() = _eventCreatedAccount
 
-    // Event which signifies the user already has an account and wants to use it
     private val _eventLoginSelected = MutableLiveData<Boolean>()
+
+    /** Event which signifies the user already has an account and wants to use it. */
     val eventLoginSelected: LiveData<Boolean>
         get() = _eventLoginSelected
 
@@ -24,26 +29,31 @@ class LoginViewModel : ViewModel() {
         _eventLoginSelected.value = false
     }
 
+    /** Signifies a new account  has been created*/
     fun createAccount() {
         // This is a placeholder for logic to validate with an api that the new account has been created
         _eventCreatedAccount.value = true
     }
 
+    /** Signifies the user has logged in with an existing account */
     fun login() {
-        // This is a placeholder to take the user to a login screen
+        // This is a placeholder to handle the user logging in with an existing account
         _eventLoginSelected.value = true
     }
 
+    /** Indicate that a new account  has been created*/
     fun onAccountCreatedComplete() {
         clearNewAccountInput()
         _eventCreatedAccount.value = false
     }
 
+    /** Indicate that the user has logged in with an existing account*/
     fun onLoginSelectedComplete() {
         clearNewAccountInput()
         _eventLoginSelected.value = false
     }
 
+    /** Clear the new account information */
     private fun clearNewAccountInput() {
         email.value = ""
         password.value = ""
